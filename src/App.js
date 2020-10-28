2b07efb1a6b98ad99d990b45f7701b53962befda
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-10-28 15:29:09 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">Jon Eriks app</div>;
+  return (
+    <div className="App">
+      Jon Eriks app
+      <a href="youtube.com">go some where</a>
+    </div>
+  );
 }
 
 export default App;
